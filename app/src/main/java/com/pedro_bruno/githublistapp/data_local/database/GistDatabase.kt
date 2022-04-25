1263f18802d6fbeb6b37c24c642dfd5bdcecb425
Pedro Bruno <pbruno1283@gmail.com>
2022-04-25 13:48:06 -0300

    SaveTypeRoom
    Criado fluxo para salvar o tipo no banco.

--- app/src/main/java/com/pedro_bruno/githublistapp/data_local/database/GistDatabase.kt
+++ app/src/main/java/com/pedro_bruno/githublistapp/data_local/database/GistDatabase.kt
@@ -2,9 +2,11 @@ package com.pedro_bruno.githublistapp.data_local.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import com.pedro_bruno.githublistapp.data_local.model.GistDataLocal
 
 @Database(entities = [GistDataLocal::class], version = 1)
+@TypeConverters(Converters::class)
 abstract class GistDatabase : RoomDatabase() {
 
     abstract fun gistDao(): GistDao
